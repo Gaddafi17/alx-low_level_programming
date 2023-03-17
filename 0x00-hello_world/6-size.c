a0ab14501ea04d0bceb196cba2fccd22d55e1cb5
Nnabuchi Chinemerem <nnabuchichinemerem@gmail.com>
2023-03-16 18:04:04 -0700

    update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d )
+Size of an int: 4 byte(s)
+Size of a long int: 8 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)
+    	return (0);
+}
