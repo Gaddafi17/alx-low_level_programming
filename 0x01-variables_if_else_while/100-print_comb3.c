66e06e12a4109dbe3dfd536d4593b46a3f08b912
Nnabuchi Chinemerem <nnabuchichinemerem@gmail.com>
2023-03-17 22:27:50 -0700

    task 10 correction

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,11 +16,11 @@
  */
 int main(void)
 {
-        int c;
+	int c;
 	int d = 0;
 
-        while (d < 10)
-        {       
+	while (d < 10)
+	{       
 		c = 0;
 		while (c < 10)
 		{
@@ -35,13 +35,13 @@ int main(void)
 					putchar(' ');
 				}
 			}
-                	c++;
-	
-        	}
+			c++;
+
+		}
 		d++;
 
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
 
 
