5d04dcbc437851d3413c85aad898bc7c9e3f0263
Nnabuchi Chinemerem <nnabuchichinemerem@gmail.com>
2023-03-17 17:16:22 -0700

    task 6 correction

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,14 @@
  */
 int main(void)
 {
-        int c = 0;
-        while (c < 10)
-        {       putchar(48 + c);
-                c++;
-        }
+	int c = 0;
+        
+	while (c < 10)
+	{       
+		putchar(48 + c);
+		c++;
+	}
 
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
