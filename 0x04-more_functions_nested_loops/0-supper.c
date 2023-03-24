89828789cca168acbcc0066de98156cddcc0244a
Nnabuchi Chinemerem <nnabuchichinemerem@gmail.com>
2023-03-23 21:55:50 -0700

    update

---
+++ 0x04-more_functions_nested_loops/0-supper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * 0-isupper.c
+ * _isupper - uppercase letters
+ * @c: char to check
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= '2')	
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * 2-mul.c
+ * mu1 - multiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
