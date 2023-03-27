7bd749104ed4c79fc1afe49b04c4fbf0de238998
Nnabuchi Chinemerem <nnabuchichinemerem@gmail.com>
2023-03-26 22:50:41 -0700

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,9 +7,10 @@
  * @c: char to check
  * Return: 0 or 1
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= '2')	
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,14 +12,15 @@ void print_line(int n)
 		_putchar('\n');
 
 	}
-	else 
+	else
+	{
 		int i;
 
-        	for (i = 1  i <= n; i++)
+        	for (i = 1;  i <= n; i++)
 		{
-			_putchar(c);
+			_putchar('_');
 		}
-		 _putchar('\');
+		 _putchar('\n');
 	}
 
-}i
+}
